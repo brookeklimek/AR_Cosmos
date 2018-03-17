f56c9eca58d1c23f24d1dc3d9a4818cb7899063b
theaphy <theheaphy@gmail.com>
2018-03-17 16:55:38 -0400

    tried 7 million drag things

---
+++ Assets/_Scripts/AllowTranslate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class AllowTranslate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit = new RaycastHit();
+
+			if(Input.GetMouseButton(0)) {
+				if(Physics.Raycast(ray, out hit)) {
+					//gameObject.GetComponent<LeanTranslate> ().enabled = true;
+					print (gameObject.name);
+				}
+			}
+		
+	}
+}
