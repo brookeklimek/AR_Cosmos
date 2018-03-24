ba569868275eb7eaa17cb4de2f4c333f7c04ec7d
theaphy <theheaphy@gmail.com>
2018-03-24 10:12:44 -0400

    pushing saturday am

---
+++ Assets/_Scripts/Orbit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public float speed;
+	public float yPosition;
+	private Vector3 pivot;
+
+	void Update () {
+		pivot = new Vector3 (0, yPosition, 0);
+		transform.RotateAround(pivot, Vector3.up, speed * Time.deltaTime);
+	}
+
+}
