69470b326ff26b27903c80496f6aa3d752834533
unknown <bcklimek@gmail.com>
2018-03-25 13:18:47 -0400

    scenes 1, 3, 5

---
+++ Assets/_Scripts/Scene3Manager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lean.Touch;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene3Manager  : MonoBehaviour {
+
+    public GameObject toScaleScene;
+    public GameObject observableScene;
+    public Text helperText;
+
+    private int view = 0;
+
+    private void OnEnable() {
+        LeanTouch.OnFingerTap += OnFingerTap;
+    }
+
+    private void OnDisable() {
+        LeanTouch.OnFingerTap -= OnFingerTap;
+    }
+
+    void Start() {
+        toScaleScene.SetActive(false);
+    }
+
+    void OnFingerTap(LeanFinger finger) {
+
+        if (finger.IsOverGui == true) {
+            return;
+        }
+
+        switch(view) {
+            case 0:
+                observableScene.SetActive(false);
+                toScaleScene.SetActive(true);
+                view = 1;
+                break;
+            case 1:
+                toScaleScene.SetActive(false);
+                observableScene.SetActive(true);
+                view = 0;
+                break;
+        }
+          helperText.text = "Fun fact"; //"Earth is in the outskirts of the galaxy. An obscure locale on the edge of a distant spiral arm.";
+        // Add helper text to tell user they can scale
+
+    }
+
+
+}
