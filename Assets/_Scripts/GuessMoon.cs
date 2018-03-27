d2f3bd97a95af53b2662b2194b87a256bc08c914
unknown <bcklimek@gmail.com>
2018-03-27 16:51:29 -0400

    test

--- Assets/_Scripts/GuessMoon.cs
+++ Assets/_Scripts/GuessMoon.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GuessMoon : MonoBehaviour {
-
+    public Transform correctPrefab;
+    public Transform incorrectPrefab;
+   // public Text resultText;
     
-
-     
-
     public void Guess() {
         if (SelectMoon.phase == gameObject.name) {
             Debug.Log("correct");
             SelectMoon.correct = true;
+
+            Instantiate(correctPrefab);
+            Destroy(correctPrefab, 3.0f);
             //"gameobject.Name" + is correct!
 
         }
         else {
             Debug.Log("incorrect");
             SelectMoon.correct = false;
+
+            Instantiate(incorrectPrefab);
+            Destroy(incorrectPrefab, 2.0f);
+
             //"Try again"
         }
     }
