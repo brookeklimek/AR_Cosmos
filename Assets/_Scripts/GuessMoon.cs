c47a926822853e0f02c6db1884a1948b63625a7f
unknown <bcklimek@gmail.com>
2018-03-27 17:29:39 -0400

    fixing scale

--- Assets/_Scripts/GuessMoon.cs
+++ Assets/_Scripts/GuessMoon.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GuessMoon : MonoBehaviour {
-    public Transform correctPrefab;
-    public Transform incorrectPrefab;
-   // public Text resultText;
+    
     
     public void Guess() {
-        if (SelectMoon.phase == gameObject.name) {
+         
+        if (RandomMoonName.phase == gameObject.name) {
             Debug.Log("correct");
-            SelectMoon.correct = true;
+            RandomMoonName.correct = true;
 
-            Instantiate(correctPrefab);
-            Destroy(correctPrefab, 3.0f);
+          
             //"gameobject.Name" + is correct!
 
         }
         else {
             Debug.Log("incorrect");
-            SelectMoon.correct = false;
+            RandomMoonName.correct = false;
 
-            Instantiate(incorrectPrefab);
-            Destroy(incorrectPrefab, 2.0f);
+            
 
             //"Try again"
         }
